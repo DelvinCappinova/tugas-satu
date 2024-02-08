800529e88544d424a9434b375eea2c9958d69b64
DelvinCappinova <delvin.cappinova@ti.ukdw.ac.id>
2024-02-08 20:55:14 +0700

    .

--- 3.py
+++ 3.py
@@ -7,4 +7,4 @@ while x <= 400000000:
   x = float(x*((100+bunga)/100))
 
 y = float(round(x))
-print (f"Perlu waktu {tahun} tahun untuk merubah Rp. 200000000 menjadi Rp {y}")
+print (f"Waktu yang dibutuhkan adalah {tahun} tahun untuk merubah Rp.200000000 menjadi Rp.{y}")
