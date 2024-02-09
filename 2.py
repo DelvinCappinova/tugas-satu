b6dcbbbfc5964aea3e31e8d94cabc8a1efb1e721
DelvinCappinova <delvin.cappinova@ti.ukdw.ac.id>
2024-02-09 17:33:10 +0700

    don

--- 2.py
+++ 2.py
@@ -4,7 +4,7 @@ C1 = 685000
 
 Uang1 = int(A1*C1 - A1*B1)
 Persen1 = float((Uang1*100)/(A1*B1))
-print (f"Emas Gerald = {A1} gram\nKeuntungan pertama Gerald = Rp.{Uang1}\nPersen keuntungan pertama Gerald = {Persen1}%")
+print (f"Emas Gerald = {A1} gram \nKeuntungan pertama Gerald = Rp.{Uang1} \nPersen keuntungan pertama Gerald = {Persen1}%")
 print ()
 
 Beli = 15
@@ -14,4 +14,4 @@ C2 = 715000
 
 Uang2 = int((A2*C2)-(A1*B1)-(Beli*B2))
 Persen2 = float((Uang2*100)/((A1*B1)+(Beli*B2)))
-print (f"Emas Gerald = {A2} gram\nKeuntungan kedua Gerald = Rp.{Uang2}\nPersen keuntungan kedua Gerald = {Persen2}%")
+print (f"Emas Gerald = {A2} gram \nKeuntungan kedua Gerald = Rp.{Uang2} \nPersen keuntungan kedua Gerald = {Persen2}%")
